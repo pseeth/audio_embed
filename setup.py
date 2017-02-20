c1beea23e8935a834d5ee39b08e4111605c79931
Prem Seetharaman <prem@u.northwestern.edu>
2017-02-19 19:10:37 -0600

    added setup.py script

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='audio_embed',
+    version='1.0.0',
+    packages=['', 'audio_embed'],
+    url='http://github.com/pseeth/audio_embed',
+    license='MIT',
+    author='prem',
+    author_email='prem@u.northwestern.edu',
+    description='Useful utilities for embedding audio into Jupyter notebooks.'
+)
